a3c3372156fef5736c9c5b402b44a7e5f57a04a8
Daniel Morales Rozas <95569595+Vanshire@users.noreply.github.com>
2025-06-26 14:50:47 -0400

    Archivos de la carpeta Login
    Permite la validación de credenciales del usuario para acceder a la plataforma, de momento sólo es una maqueta

---
+++ login.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let currentSlide = 0;
+    const slides = document.querySelectorAll('.slide');
+
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            if (i === index) {
+                slide.style.display = 'block';
+            } else {
+                slide.style.display = 'none';
+            }
+        });
+    }
+
+    function nextSlide() {
+        currentSlide = (currentSlide + 1) % slides.length;
+        showSlide(currentSlide);
+    }
+
+    setInterval(nextSlide, 3000); // Cambia de slide cada 3 segundos
+
+    // Mostrar el primer slide al cargar la página
+    showSlide(currentSlide);
+});
